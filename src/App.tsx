1d16e6d020c341acc32f232d71cb711be0717d1c
Dandi Darmawan <mdandi.darmawan@gmail.com>
2021-09-23 22:34:04 +0700

    Update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
+import NumberFormat from 'react-number-format';
 import { Ticker } from './Ticker';
 
 interface CardProps {
   tickerData: Ticker;
 }
 
-let decimalDigit = 5;
-decimalDigit = 10 ** decimalDigit;
-
-// To be changed
-const client = new W3CWebSocket('wss://0e40b8a50ac5.eu.ngrok.io/');
+const DECIMAL = 5;
+const client = new W3CWebSocket(process.env.REACT_APP_WEBSOCKET_CLIENT ?? '');
 
 const Card = (props: CardProps) => {
   const { tickerData } = props;
-  const tickerVolume = Math.round(parseFloat(tickerData.ticker.volume) * decimalDigit) / decimalDigit;
-  const tickerChange = Math.round(parseFloat(tickerData.ticker.change) * decimalDigit) / decimalDigit;
 
   return (
     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2 mb-1">
       <div className="w-full rounded overflow-hidden border">
         <div className="px-6 py-4">
           <div className="font-bold text-3xl mb-1">{tickerData.name}</div>
-          <p className="text-yellow-500 text-xl mb-4">
-            ${tickerData.ticker.price}
-          </p>
-          <table className="text-gray-500 table-fixed text-sm">
+            <NumberFormat
+              value={tickerData.ticker.price}
+              className="text-yellow-500 text-xl mb-4"
+              displayType={'text'}
+              decimalScale={DECIMAL}
+              thousandSeparator={true}
+              prefix={'$'}
+            />
+          <table className="text-gray-500 table-fixed text-sm mt-4  ">
             <thead>
-              <tr>
-                <td className="w-1/3">volume:</td>
-                <td className="w-1/3"></td>
-                <td className="w-1/3">change:</td>
+              <tr className="w-full">
+                <td className="w-1/6">volume:</td>
+                <td className="w-1/6">change:</td>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>${tickerVolume}</td>
-                <td></td>
-                <td className={tickerChange < 0 ? 'text-red-500' : 'text-green-500'}>{tickerChange}%</td>
+                <td className="w-1/6">
+                  {tickerData.ticker.volume ?
+                    <NumberFormat
+                      value={tickerData.ticker.volume}
+                      defaultValue='-'
+                      displayType={'text'}
+                      decimalScale={DECIMAL}
+                      thousandSeparator={true}
+                      prefix={'$'}
+                    />
+                  : '-'}
+                </td>
+                <td className="w-1/6">
+                  <NumberFormat
+                    value={tickerData.ticker.change}
+                    defaultValue='-'
+                    className={parseFloat(tickerData.ticker.change) < 0 ? 'text-red-500' : 'text-green-500'}
+                    displayType={'text'}
+                    decimalScale={DECIMAL}
+                    thousandSeparator={true}
+                    suffix={'%'}
+                  />
+                </td>
               </tr>
             </tbody>
           </table>
@@ -49,13 +69,14 @@ const Card = (props: CardProps) => {
 
 function App() {
   const [tickersData, setTickersData] = useState<Ticker[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     client.onmessage = (message) => {
       const { data } = message;
-      const tickersData = JSON.parse(data as string) as Ticker[];
-      setTickersData(tickersData);
-      console.log(tickersData)
+      const tickers = JSON.parse(data as string) as Ticker[];
+      setTickersData(tickers);
+      setLoading(false);
     };
   }, []);
 
@@ -65,9 +86,12 @@ function App() {
         <span className="font-semibold text-4xl">Cryptocurrency Realtime Price</span>
       </div>
 
-      {tickersData.map((tickerData, index) => {
-        return tickerData.success ? <Card key={index} tickerData={tickerData} /> : '';
-      })}
+      {loading ? <span className="text-lg">Loading...</span>
+        : tickersData.map((tickerData, index) => {
+          return tickerData.success ? <Card key={index} tickerData={tickerData} /> : '';
+        })
+      }
+
     </div>
   );
 }
